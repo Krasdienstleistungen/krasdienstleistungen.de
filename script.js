b2347f13fcb9afa1d5226f1d187cf2d626db1050
Krasdienstleistungen <info@krasdienstleistungen.de>
2026-06-03 12:12:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ window.addEventListener('scroll', () => {
 
     const scroll = window.scrollY;
 
-    const scale = Math.max(0.4, 1 - scroll / 800);
+    const scale = Math.max(0.3, 1 - scroll / 500);
 
-    const moveY = scroll * 0.8;
+    const moveY = scroll * 1.5;
 
     logo.style.transform =
         `translateY(-${moveY}px) scale(${scale})`;
+
+    logo.style.opacity =
+        Math.max(0, 1 - scroll / 400);
 
 });
